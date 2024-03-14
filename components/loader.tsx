a5e6419c2266b377ef476e0caa0a6fb4d0560f78
jsl1114 <ljs11141118@gmail.com>
2024-03-14 15:28:09 -0400

    add styles

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export const Loader = () => {
   return (
     <div className='h-full flex flex-col gap-y-4 items-center justify-center'>
-      <div className='w-10 h-10 relative animate-bounce'>
+      <div className='w-20 h-20 relative animate-bounce'>
         <Image
           alt='logo'
           fill
